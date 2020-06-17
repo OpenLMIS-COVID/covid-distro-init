8826b55f6e3ade4154a51c9030f1a64ed1ef913a
Paulina Buzderewicz <pbuzderewicz@soldevelo.com>
2020-06-17 10:20:00 +0200

    COV-108: Updated psql statement

--- add_pcmt_right.sh
+++ add_pcmt_right.sh
@@ -22,10 +22,9 @@ DB=`echo ${DATABASE_URL} | sed -E 's/^.*\/(.+)\?*$/\1/'` # /<db>?
 : "${DB:?DB not set}"
 
 # pgpassfile makes it easy and safe to login
-printenv
 echo "${HOST}:${PORT}:${DB}:${POSTGRES_USER}:${POSTGRES_PASSWORD}" > pgpassfile
 chmod 600 pgpassfile
 
 # execute query
 export PGPASSFILE='pgpassfile'
-psql "${URL}" -U ${POSTGRES_USER} -t -c "INSERT INTO referencedata.rights (id, description, name, type) VALUES('5920d0eb-c2df-411f-9d16-bf1e9b745bd9', NULL, 'PCMT_MANAGEMENT', 'GENERAL_ADMIN') ON CONFLICT DO NOTHING;" | psql "${URL}" -U ${POSTGRES_USER} 2>&1
+psql "${URL}" -U ${POSTGRES_USER} -t -c "INSERT INTO referencedata.rights (id, description, name, type) VALUES('5920d0eb-c2df-411f-9d16-bf1e9b745bd9', NULL, 'PCMT_MANAGEMENT', 'GENERAL_ADMIN') ON CONFLICT DO NOTHING;"
